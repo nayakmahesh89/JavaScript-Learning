96e74ef4621ee80422d0c2b6717213e3207ceee8
nayakmahesh89 <nayakmahesh89@hotmail.com>
2025-07-30 19:59:38 +0000

    Con

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+    const AccountId = 1234345
+    let AccountEmail = "mahesh@cloudsharks.io"
+    var AccountPassword = 'password123'
+    
+    
+    AccountName = 'Mahesh Nayak'
+
+    // AccountId = 345678
+    AccountEmail = 'naveen@cloudsharks.io'
+    AccountPassword = 'password345'
+
+    console.log(AccountName)
+    let AccountCity
+    console.table([AccountEmail, AccountId, AccountPassword, AccountName, AccountCity])
